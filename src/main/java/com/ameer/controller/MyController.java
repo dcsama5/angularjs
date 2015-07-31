d070f80f3ff7c73c93a3e8aeb474eaef6122b80c
dcsama5 <dcs.ama5@gmail.com>
2015-07-31 20:05:49 +0100

    Update to almost finish chapter 1 i think

--- src/main/java/com/ameer/controller/MyController.java
+++ src/main/java/com/ameer/controller/MyController.java
@@ -1,15 +1,23 @@
 package com.ameer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MyController {
-	 
+
 	 @RequestMapping(value="/person", method=RequestMethod.GET)
 	 public String returnPage() {
 		 return "person";
 	 }
 	 
+	 @RequestMapping(value="/todo")
+	 public String todoPageChapter1() {
+		 return "todoApp";
+	 }
+	
 }
